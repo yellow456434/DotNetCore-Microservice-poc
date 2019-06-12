e6bdb8418d7b4a2c4d30dad091ffccbe3e4bee86
willy <yellow456434@hotmail.com>
2019-06-12 18:10:51 +0800

    設定consul 服務註冊

--- productService/Startup.cs
+++ productService/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿
+using System;
+using Consul;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -8,6 +11,15 @@ using productService.Models;
 
 namespace productService
 {
+    public class ServiceEntity
+    {
+        public string IP { get; set; }
+        public int Port { get; set; }
+        public string ServiceName { get; set; }
+        public string ConsulIP { get; set; }
+        public int ConsulPort { get; set; }
+    }
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -28,7 +40,7 @@ namespace productService
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime lifetime)
         {
             if (env.IsDevelopment())
             {
@@ -41,6 +53,52 @@ namespace productService
 
             //app.UseHttpsRedirection();
             app.UseMvc();
+
+            ServiceEntity serviceEntity = new ServiceEntity
+            {
+                IP = Configuration["Consul:IP"],
+                Port = Convert.ToInt32(Configuration["Service:Port"]),
+                ServiceName = Configuration["Service:Name"],
+                ConsulIP = Configuration["Consul:IP"],
+                ConsulPort = Convert.ToInt32(Configuration["Consul:Port"])
+            };
+            app.RegisterConsul(lifetime, serviceEntity);
+        }
+    }
+
+    public static class AppBuilderExtensions
+    {
+        public static IApplicationBuilder RegisterConsul(this IApplicationBuilder app, IApplicationLifetime lifetime, ServiceEntity serviceEntity)
+        {
+            var httpCheck = new AgentServiceCheck()
+            {
+                DeregisterCriticalServiceAfter = TimeSpan.FromSeconds(5),//服务启动多久后注册
+                Interval = TimeSpan.FromSeconds(10),//健康检查时间间隔，或者称为心跳间隔
+                HTTP = $"http://{serviceEntity.IP}:{serviceEntity.Port}/api/values",//健康检查地址
+                Timeout = TimeSpan.FromSeconds(5)
+            };
+
+            // Register service with consul
+            var registration = new AgentServiceRegistration()
+            {
+                Checks = new[] { httpCheck },
+                ID = Guid.NewGuid().ToString(),
+                Name = serviceEntity.ServiceName,
+                Address = serviceEntity.IP,
+                Port = serviceEntity.Port,
+                //Tags = new[] { $"urlprefix-/{serviceEntity.ServiceName}" }//添加 urlprefix-/servicename 格式的 tag 标签，以便 Fabio 识别
+            };
+
+            var consulClient = new ConsulClient(x => x.Address = new Uri("http://" + serviceEntity.ConsulIP + ":" + serviceEntity.ConsulPort));//请求注册的 Consul 地址
+
+            consulClient.Agent.ServiceRegister(registration).Wait();//服务启动时注册，内部实现其实就是使用 Consul API 进行注册（HttpClient发起）
+
+            lifetime.ApplicationStopping.Register(() =>
+                {
+                    consulClient.Agent.ServiceDeregister(registration.ID).Wait();//服务停止时取消注册
+            });
+
+            return app;
         }
     }
 }
