eb16ff25e6a5b41ea5e6b3f43ade4d6ac78be10f
willy <yellow456434@hotmail.com>
2019-11-21 11:34:14 +0800

    add IdentityServer4

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,106 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids (){
+
+            var customProfile = new IdentityResource(
+            name: "custom.profile",
+            displayName: "Custom profile",
+            claimTypes: new[] { "location" });
+
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                customProfile
+            };
+        }
+
+        public static IEnumerable<ApiResource> Apis =>
+            new ApiResource[]
+            {
+                new ApiResource("api1", "My API"),
+                new ApiResource{
+                    Name = "api2",
+                    DisplayName = "api2 Display",
+                    // secret for using introspection endpoint
+                    ApiSecrets =
+                    {
+                        new Secret("api2".Sha256())
+                    },
+
+                    // include the following using claims in access token (in addition to subject id)
+                    //UserClaims = { JwtClaimTypes.Name, JwtClaimTypes.Email },
+
+                    // this API defines two scopes
+                    Scopes =
+                    {
+                        new Scope()
+                        {
+                            Name = "api2.full_access",
+                            DisplayName = "Full access to API 2",
+                        }
+                    }
+                }
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]{
+                new Client {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                },
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireConsent = true,
+                    RequirePkce = false,
+                    AllowOfflineAccess = true,
+                    AccessTokenType = AccessTokenType.Reference,
+
+                    RedirectUris = { "http://localhost:5002/signin-oidc" },
+
+                    AllowAccessTokensViaBrowser = true,
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.OfflineAccess,
+                        "custom.profile",
+                        "api2.full_access"
+                    },
+
+                    AuthorizationCodeLifetime = 300,
+                    AccessTokenLifetime = 300,
+                    IdentityTokenLifetime = 300,
+                    RefreshTokenExpiration = TokenExpiration.Absolute,
+                    AbsoluteRefreshTokenLifetime = 3600
+                }
+            };
+
+    }
+}
---
+++ IdentityServer/Startup.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
+        {
+            Environment = environment;
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // uncomment, if you want to add an MVC-based UI
+            services.AddControllersWithViews();
+
+            var builder = services.AddIdentityServer((otp) =>
+                {
+                    otp.UserInteraction = new IdentityServer4.Configuration.UserInteractionOptions()
+                    {
+                        //LoginUrl = "http://www.google.com"
+                        //ConsentUrl = "http://www.google.com",
+                    };
+                    otp.Endpoints = new IdentityServer4.Configuration.EndpointsOptions()
+                    {
+                        EnableCheckSessionEndpoint = false,
+                        EnableEndSessionEndpoint = false,
+                        EnableDeviceAuthorizationEndpoint = false
+                    };
+                })
+                //.AddConfigurationStore(options =>
+                //{
+                //    options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+                //        sql => sql.MigrationsAssembly(migrationsAssembly));
+                //})
+                //.AddOperationalStore(options =>
+                //{
+                //    options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+                //    sql => sql.MigrationsAssembly(migrationsAssembly));
+                //})
+                .AddTestUsers(TestUsers.Users);
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // uncomment if you want to add MVC
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseIdentityServer();
+
+            // uncomment, if you want to add MVC
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
