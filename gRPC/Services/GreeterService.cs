a2ed32353375c8dbdaaab3ba2b4a41bf69bd13d0
willy <yellow456434@hotmail.com>
2020-01-17 17:34:27 +0800

    gRPC

---
+++ gRPC/Services/GreeterService.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace gRPC
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            Console.WriteLine(request.Birthday2.ToDateTime());
+
+            var tt = request.Birthday2.ToDateTime().AddDays(1);
+
+            System.Threading.Thread.Sleep(5000);
+
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name,
+                Birthday2 = Timestamp.FromDateTime(tt.ToUniversalTime())
+            });
+        }
+    }
+}
---
+++ gRPCClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using gRPC;
+using Grpc.Net.Client;
+
+namespace gRPCClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Send Rpc");
+            AppContext.SetSwitch(
+    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            using var channel = GrpcChannel.ForAddress("http://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient", Birthday2 = Timestamp.FromDateTime(DateTime.Now.ToUniversalTime()) });
+            Console.WriteLine("Greeting: " + reply.Message); 
+            Console.WriteLine("Bitthdat: " + reply.Birthday2.ToDateTime());
+            Console.WriteLine("exit...");
+            //Console.ReadKey();
+        }
+    }
+}
